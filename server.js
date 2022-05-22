f74e6bd4f5e9931097dcf36192fc6f44835721a3
Nicky <fatemesahraee2000@gmail.com>
2022-05-22 13:45:52 +0430

    some modifications

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors({
     //TODO: localhost to the server hosting front-end ip
-    origin: 'http://localhost:3005',
+    origin: 'http://185.110.189.42:3005',
     credentials: true
 }));
 
